b26922273fa28cc867d9b0f8af390131de53508b
Rastre <rastor13666@gmail.com>
2025-11-10 13:02:14 +0300

    added connection settings

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,29 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/testbase";
+    private static final String LOGIN = "root";
+    private static final String PASSWORD = "i@dijwfdqa2";
+    private static Connection connection;
+
+    private static Connection getConnection() {
+        try {
+            connection = DriverManager.getConnection(URL, LOGIN, PASSWORD);
+        } catch (SQLException e) {
+            System.out.println("Ошибка подключения: " + e.getMessage());
+        }
+        return connection;
+    }
+
+    public static void closeConnection() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            System.out.println("Ошибка закрытия: " + e.getMessage());
+        }
+    }
 }
